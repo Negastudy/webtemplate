3a9c155da3bb72379f58332463049839ba958459
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-11-29 14:07:43 +0900

    로그인화면 완료 (폴더 정리x)

--- html5up-phantom (1)/assets/js/scripts.js
+++ html5up-phantom (1)/assets/js/scripts.js
@@ -8,7 +8,10 @@ jQuery(document).ready(function() {
                     "assets/img/backgrounds/2.jpg"
 	              , "assets/img/backgrounds/3.jpg"
 	              , "assets/img/backgrounds/1.jpg"
-	             ], {duration: 3000, fade: 750});
+					, "assets/img/backgrounds/4.jpg"
+					, "assets/img/backgrounds/5.jpg"
+        , "assets/img/backgrounds/6.jpg"
+	             ], {duration: 6000, fade: 550});
     
     /*
         Form validation
